7161be8a49bb63f6ab1d5b9ba07e5f9e13428021
infinitron <karthikreddy311@gmail.com>
2021-11-04 15:21:39 -0400

    adds correct py version and moodule name

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setup(
     cmake_install_dir="src/pylira_simd",
     include_package_data=True,
     extras_require={"test": ["pytest"]},
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
