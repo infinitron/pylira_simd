f32f3a3f1779e8710b7aeade52caf47dbe280df9
infinitron <karthikreddy311@gmail.com>
2021-11-08 11:13:44 -0500

    added test run with floating precision

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,21 +20,29 @@ def test_exports_image_analysis():
 def test_exports_data():
     assert m.get_sample_images() is not None
 
+
 def test_test_payload():
     test_payload = m.get_test_payload()
     sample_images = m.get_sample_images()
-    assert (test_payload.observation==sample_images.img_64x64).all()
-    assert (test_payload.psf==sample_images.psf_33x33).all()
-    assert (test_payload.baseline==sample_images.baseline_64x64).all()
-    assert (test_payload.exp_map==sample_images.expmap_64x64).all()
-    assert (test_payload.start_map==sample_images.start_64x64).all()
+    assert (test_payload.observation == sample_images.img_64x64).all()
+    assert (test_payload.psf == sample_images.psf_33x33).all()
+    assert (test_payload.baseline == sample_images.baseline_64x64).all()
+    assert (test_payload.exp_map == sample_images.expmap_64x64).all()
+    assert (test_payload.start_map == sample_images.start_64x64).all()
+
 
 def test_run_lira():
     test_payload = m.get_test_payload()
-    test_payload.max_iter=5
-    test_payload.burn_in=1
-    test_payload.thin=1
-    test_payload.out_param_file="out.param"
-    test_payload.out_img_file="img.out"
-    #check if it runs without exceptions
+    test_payload.max_iter = 5
+    test_payload.burn_in = 1
+    test_payload.thin = 1
+    test_payload.out_param_file = "out.param"
+    test_payload.out_img_file = "img.out"
+    test_payload.use_float = False
+    # check if it runs without exceptions
     assert test_payload.launch_image_analysis() is not None
+
+    # try it with float precision
+    test_payload.use_float = True
+    assert test_payload.launch_image_analysis() is not None
+
